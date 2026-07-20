a09cb93e93fca003d112f866638475f305fb917f
Sushant <vermashushant18@gmail.com>
2026-07-20 23:35:39 +0530

    Lesson 11 Theory

--- lesson 11/11-todo-list.js
+++ lesson 11/11-todo-list.js
@@ -1,10 +1,23 @@
-let array = [];
-function addTodo() {
-let inputElement = document.querySelector('.js-todo-input');
-let todoInput = inputElement.value;
+let todoList = [ ];
 
-array.push(todoInput);
-console.log(array);
+renderTodoList();
 
-inputElement.value = '';
+function renderTodoList(){
+  let todoListHtml = '';
+  for(let i = 0; i < todoList.length; i++){
+  const todo = todoList[i];
+  todoListHtml += `<p>${todo}</p>`;
+  }
+  document.querySelector('.js-todo-container').innerHTML = todoListHtml;
 }
+
+
+function addTodo(){
+  let innerElement = document.querySelector('.js-todo-input');
+  let todoInput = innerElement.value;
+  todoList.push(todoInput);
+  console.log(todoList);
+  document.querySelector('.js-todo-input').value = '';
+
+  renderTodoList();
+} 
